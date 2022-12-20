16afbbba2ab8a2e6ee42ff28b38dfe92c879eb32
farleyaAraujoLeite <farley2181@hotmail.com>
2022-12-20 18:23:49 -0300

    instalando o body-parser e configurando para receber os dados do formulario no back-end.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //renderizador de html
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+//o body parser vai decodificar os dados enviados pelo formulário
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json());
+
+//rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+//rota criada para receber os dados enviados pelo formulário
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.título;
+  var descricao = req.body.descricao;
+  res.send(`Formulário recebido! Titulo: ${titulo} Descrição: ${descricao}`);
 });
 
 
